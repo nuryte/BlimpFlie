fc24b25b21885021d69e63cc017dbf089493e38d
nuryte <positronic83@gmail.com>
2023-10-20 16:18:59 -0400

    transeiver clear

--- ModSender/ModTransiever.py
+++ ModSender/ModTransiever.py
@@ -4,7 +4,7 @@ from robotConfig import RobotConfig
 import time
 
 #ESPNOW PARAMS
-ESP_VERBOSE = True
+ESP_VERBOSE = False
 PORT = "COM5"
 LIST_OF_MAC_ADDRESS = [
     "34:85:18:91:BC:94",
@@ -40,7 +40,7 @@ robConfig.sendAllFlags(BRODCAST_CHANNEL, SLAVE_INDEX, "bicopterbasic")
 
 # robConfig.startBNO(BRODCAST_CHANNEL, SLAVE_INDEX)
 # robConfig.startBaro(BRODCAST_CHANNEL, SLAVE_INDEX)
-# robConfig.startTranseiver(BRODCAST_CHANNEL, SLAVE_INDEX, MASTER_MAC)
+robConfig.startTranseiver(BRODCAST_CHANNEL, SLAVE_INDEX, MASTER_MAC)
 
 
 y = False
@@ -50,9 +50,9 @@ try:
         outputs, y = joyhandler.get_outputs()
         
         esp_now.send([21] + outputs[:-1], BRODCAST_CHANNEL, SLAVE_INDEX)
-        #flag, feedback  = esp_now.getFeedback()
-        #print(feedback)
-        #esp_now.send(outputs, BRODCAST_CHANNEL, 0)
+        flag, feedback  = esp_now.getFeedback()
+        print(feedback)
+        esp_now.send(outputs, BRODCAST_CHANNEL, 0)
         time.sleep(0.02)
 except KeyboardInterrupt:
     print("Loop terminated by user.")
