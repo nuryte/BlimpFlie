f9ac765317294aa7b7fdddb474b4e9b11b1577e3
nuryte <positronic83@gmail.com>
2023-10-24 00:07:43 -0400

    updated feedback from new capabilty

--- ModSender/ModTransiever.py
+++ ModSender/ModTransiever.py
@@ -4,7 +4,7 @@ from robotConfig import RobotConfig
 import time
 
 #ESPNOW PARAMS
-ESP_VERBOSE = False
+ESP_VERBOSE = True
 PORT = "COM5"
 LIST_OF_MAC_ADDRESS = [
     "34:85:18:91:BC:94",
@@ -23,7 +23,7 @@ LIST_OF_MAC_ADDRESS = [
 
 MASTER_MAC = "34:85:18:91:C7:80" #address of the transiever
 
-SLAVE_INDEX = 5 #-1 means broadcast
+SLAVE_INDEX = 2 #-1 means broadcast
 
 
 BRODCAST_CHANNEL = 1 # SLAVE_INDEX will override this value if SLAVE_INDEX is not -1
@@ -48,11 +48,11 @@ try:
     while not y:
 
         outputs, y = joyhandler.get_outputs()
-        
+        #outputs = [0]*13
+        feedback  = esp_now.getFeedback()
         esp_now.send([21] + outputs[:-1], BRODCAST_CHANNEL, SLAVE_INDEX)
-        flag, feedback  = esp_now.getFeedback()
-        print(feedback)
-        esp_now.send(outputs, BRODCAST_CHANNEL, 0)
+        #print(feedback)
+        
         time.sleep(0.02)
 except KeyboardInterrupt:
     print("Loop terminated by user.")
