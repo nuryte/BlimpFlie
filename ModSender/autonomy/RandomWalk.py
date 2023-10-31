725b2b7234306934236d1b11cf44a5ee6c248110
David <dajulian@gmail.com>
2023-10-31 02:14:36 -0400

    state machine for random walk

--- ModSender/autonomy/RandomWalk.py
+++ ModSender/autonomy/RandomWalk.py
@@ -5,44 +5,67 @@ import numpy as np
 from ModSender.autonomy.Autonomous import Autonomous
 
 
+
+
 class RandomWalk(Autonomous):
 
-    def __init__(self):
-        self.x_force = 0.3
-        self.des_yaw = 0
-        self.des_z = 4
+    def __init__(self, forward_force=0.3, min_distance=400, des_z=4):
+        # Constants
+        self.forward_force = forward_force
+        self.min_distance = min_distance
+        self.des_z = des_z
+        self.time_backward = 2
+        self.time_rotate = 2
 
-        self.counter = 100
-        self.flag = False
+        # Variable
+        self.des_yaw = 0
+
+        # current action
+        self.current_action = 0
+
+        # actions
+        self.actions = [self._action_move_forward, self._action_move_backward, self._action_rotate]
+
     def begin(self):
-        pass
+        self._restart_timer()
+
 
 
     def execute(self, feedback):
 
-          # constant x force to move forward
-
+        # Variables to make decisions
         distance = feedback[5]  # Distance from the sonar
+        time_elapsed = self._time_elapsed()
 
-        if distance < 400:  # if distance is less than 400 cm then turn
-            time_elapse = time.time()  # save the current time
-            # angle_r = np.random.uniform(0, np.pi) + np.pi/2 # random value in the opposite direction
-            # joyhandler.tz += angle_r # modify the angle to send
-            # self.des_yaw += np.pi
+        # ---------- Switch actions based on timer and distance -----------
+        if self.current_action == 0 and distance < self.min_distance:
+            self.current_action = 1  # Move backwards
+            self._restart_timer()
 
-            self.x_force = -0.3
-            self.counter = 0
-            self.flag = True
+        elif self.current_action == 1 and time_elapsed > self.time_backward:
+            self.current_action = 2  # Rotate
+            self._restart_timer()
 
-        if self.flag and self.counter > 25:
-            self.des_yaw += np.random.uniform(0, np.pi) + np.pi/2
-            self.x_force = 0.3
-            self.counter = 0
-            self.flag = False
+        elif self.current_action == 2 and time_elapsed > self.time_rotate:
+            self.current_action = 0  # Move forward
 
-        print(self.counter)
-        self.counter+=1
+        # Select the current action function
+        action = self.actions[self.current_action]
 
-        des_fx, des_z, des_yaw =  self.x_force, self.des_z, self.des_yaw
+        return action()
 
-        return des_fx, des_z, des_yaw
+    def _restart_timer(self):
+        self.time_elapse = time.time()
+
+    def _time_elapsed(self):
+        return time.time() - self.time_elapse
+
+    def _action_move_forward(self):
+        return self.forward_force, self.des_yaw, self.des_z
+
+    def _action_move_backward(self):
+        return -self.forward_force, self.des_yaw, self.des_z
+
+    def _action_rotate(self):
+        self.des_yaw += np.random.uniform(0, np.pi) + np.pi / 2
+        return self.forward_force, self.des_yaw, self.des_z
